cc77aaa085305bd4ba18f0e36461662097b9a952
Mr.G8 <34008592+JordanG8@users.noreply.github.com>
2026-01-07 14:35:57 +0200

    .env  fix

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -3,7 +3,8 @@
  * טוען משתני סביבה מקובץ .env
  */
 
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
 
 const config = {
   // הגדרות שרת
